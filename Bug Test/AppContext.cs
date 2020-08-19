6755fb724f6e2e0af04ce9619d369253b3dcbb8c
Dmitry Yuzefchik <d.yuzefchik@gmail.com>
2020-08-19 12:30:48 +0300

    Init

---
+++ Bug Test/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bug_Test
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
---
+++ Bug Test/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Bug_Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly AppContext _context;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, AppContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.WeatherForecasts.ToListAsync());
+        }
+
+        [HttpPost]
+        [Produces("application/json")]
+        public async Task<IActionResult> Post()
+        {
+            var rng = new Random();
+            _context.WeatherForecasts.Add(new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+            await _context.SaveChangesAsync();
+            return Ok("done");
+        }
+    }
+}
